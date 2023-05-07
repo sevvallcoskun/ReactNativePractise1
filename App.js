4d142605a5aacd823e67766f25affb205eb18b2e
sevvallcoskun <asevval.coskun@istanbulticaret.edu.tr>
2023-05-07 20:58:35 +0300

    practise2

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
+import React,{useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const[people,setPeople]=useState([
+    {name:'sevval',id:'1'},
+    {name:'bahar',id:'2'},
+    {name:'sencer',id:'3'},
+    {name:'miray',id:'4'},
+    {name:'berke',id:'5'},
+    {name:'mehlika',id:'6'},
+  ]);
+  const pressHandler=(id)=>{
+    console.log(id);
+    setPeople((prevPeople)=>{
+      return prevPeople.filter(person=>person.id!=id);
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        keyExtractor={(item)=>item.id}
+        data={people}
+        renderItem={({item})=>(
+          <TouchableOpacity onPress={()=>pressHandler(item.id)}>
+            <Text style={styles.item}>{item.name}</Text>
+          </TouchableOpacity>
+          
+        )}
+      />
+      {/* <ScrollView>
+        {people.map((item)=>{
+          return (
+            <View key={item.key}>
+              <Text style={styles.item}>{item.name}</Text>
+            </View>
+          )
+        })}
+      </ScrollView> */}
     </View>
   );
 }
@@ -14,7 +47,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop:40,
+    paddingHorizontal:20
   },
+  item:{
+    marginTop:24,
+    padding:30,
+    backgroundColor:'pink',
+    fontSize:24
+  }
 });
+
+
